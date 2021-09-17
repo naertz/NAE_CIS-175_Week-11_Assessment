5a5c49123191083d5bb9d64b30070e9e1bf66585
Noah Allan Ertz <naertz@dmacc.edu>
2021-09-17 01:06:57 -0500

    Initial commit

---
+++ src/controller/LinuxDistroHelper.java
@@ -0,0 +1,119 @@
+/**
+ * @author NAE (Noah Ertz) - naertz
+ * CIS-175 - Fall 2021
+ * Sep 16, 2021
+ */
+
+package controller;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import model.LinuxDistro;
+
+public class LinuxDistroHelper {
+    private static EntityManagerFactory linuxDistributionsEMF = Persistence.createEntityManagerFactory("LinuxDistributions");
+    
+    public LinuxDistro findLinuxDistroID(int id) {
+        EntityManager linuxDistributionsEM = linuxDistributionsEMF.createEntityManager();
+        linuxDistributionsEM.getTransaction().begin();
+        
+        LinuxDistro foundDistro = linuxDistributionsEM.find(LinuxDistro.class, id);
+        
+        linuxDistributionsEM.close();
+        return foundDistro;
+    }
+    
+    public List<LinuxDistro> findLinuxDistroName(String name) {
+        EntityManager linuxDistributionsEM = linuxDistributionsEMF.createEntityManager();
+        linuxDistributionsEM.getTransaction().begin();
+        TypedQuery<LinuxDistro> linuxDistroTypedQuery = linuxDistributionsEM.createQuery("SELECT distro FROM LinuxDistro distro WHERE distro.name = :selectedName", LinuxDistro.class);
+        
+        linuxDistroTypedQuery.setParameter("selectedName", name);
+        
+        List<LinuxDistro> distroResults = linuxDistroTypedQuery.getResultList();
+        
+        linuxDistributionsEM.close();
+        return distroResults;
+    }
+    
+    public List<LinuxDistro> findLinuxDistroBasedOn(String basedOn) {
+        EntityManager linuxDistributionsEM = linuxDistributionsEMF.createEntityManager();
+        linuxDistributionsEM.getTransaction().begin();
+        TypedQuery<LinuxDistro> linuxDistroTypedQuery = linuxDistributionsEM.createQuery("SELECT distro FROM LinuxDistro distro WHERE distro.basedOn = :selectedBasedOn", LinuxDistro.class);
+        
+        linuxDistroTypedQuery.setParameter("selectedBasedOn", basedOn);
+        
+        List<LinuxDistro> distroResults = linuxDistroTypedQuery.getResultList();
+        
+        linuxDistributionsEM.close();
+        return distroResults;
+    }
+    
+    public List<LinuxDistro> findLinuxDistroOrigin(String origin) {
+        EntityManager linuxDistributionsEM = linuxDistributionsEMF.createEntityManager();
+        linuxDistributionsEM.getTransaction().begin();
+        TypedQuery<LinuxDistro> linuxDistroTypedQuery = linuxDistributionsEM.createQuery("SELECT distro FROM LinuxDistro distro WHERE distro.origin = :selectedOrigin", LinuxDistro.class);
+        
+        linuxDistroTypedQuery.setParameter("selectedOrigin", origin);
+        
+        List<LinuxDistro> distroResults = linuxDistroTypedQuery.getResultList();
+        
+        linuxDistributionsEM.close();
+        return distroResults;
+    }
+    
+    public void insertLinuxDistro(LinuxDistro distro) {
+        EntityManager linuxDistributionsEM = linuxDistributionsEMF.createEntityManager();
+        linuxDistributionsEM.getTransaction().begin();
+        
+        linuxDistributionsEM.persist(distro);
+        linuxDistributionsEM.getTransaction().commit();
+        linuxDistributionsEM.close();
+    }
+    
+    public void updateLinuxDistro(LinuxDistro distro) {
+        EntityManager linuxDistributionsEM = linuxDistributionsEMF.createEntityManager();
+        linuxDistributionsEM.getTransaction().begin();
+        
+        linuxDistributionsEM.merge(distro);
+        linuxDistributionsEM.getTransaction().commit();
+        linuxDistributionsEM.close();
+    }
+    
+    public void deleteLinuxDistro(LinuxDistro distro) {
+        EntityManager linuxDistributionsEM = linuxDistributionsEMF.createEntityManager();
+        linuxDistributionsEM.getTransaction().begin();
+        TypedQuery<LinuxDistro> linuxDistroTypedQuery = linuxDistributionsEM.createQuery("SELECT distro FROM LinuxDistro distro WHERE distro.name = :selectedName AND distro.basedOn = :selectedBasedOn AND distro.origin = :selectedOrigin", LinuxDistro.class);
+        
+        linuxDistroTypedQuery.setParameter("selectedName", distro.getName());
+        linuxDistroTypedQuery.setParameter("selectedBasedOn", distro.getBasedOn());
+        linuxDistroTypedQuery.setParameter("selectedOrigin", distro.getOrigin());
+        
+        linuxDistroTypedQuery.setMaxResults(1);
+        
+        LinuxDistro distroResult = linuxDistroTypedQuery.getSingleResult();
+        
+        linuxDistributionsEM.remove(distroResult);
+        linuxDistributionsEM.getTransaction().commit();
+        linuxDistributionsEM.close();
+    }
+    
+    public List<LinuxDistro> getLinuxDistroList() {
+        EntityManager linuxDistributionsEM = linuxDistributionsEMF.createEntityManager();
+        TypedQuery<LinuxDistro> linuxDistroTypedQuery = linuxDistributionsEM.createQuery("SELECT distro FROM LinuxDistro distro", LinuxDistro.class);
+        
+        List<LinuxDistro> linuxDistros = linuxDistroTypedQuery.getResultList();
+        
+        linuxDistributionsEM.close();
+        return linuxDistros;
+    }
+    
+    public void closeEMF() {
+        linuxDistributionsEMF.close();
+    }
+}
---
+++ src/model/LinuxDistro.java
@@ -0,0 +1,97 @@
+/**
+ * @author NAE (Noah Ertz) - naertz
+ * CIS-175 - Fall 2021
+ * Sep 16, 2021
+ */
+
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="LinuxDistributions")
+public class LinuxDistro {
+    // Instance Variable(s) ===========================
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="ID")
+    private int id;         // ID
+    @Column(name="Name")
+    private String name;    // Name
+    @Column(name="BasedOn")
+    private String basedOn; // Based On
+    @Column(name="Origin")
+    private String origin;  // Origin
+    // ================================================
+    
+    // Constructor(s) ==============================================
+    // Default
+    public LinuxDistro() {
+        super();
+    }
+    
+    // Name, Based On, Origin
+    public LinuxDistro(String name, String basedOn, String origin) {
+        super();
+        this.name = name;
+        this.basedOn = basedOn;
+        this.origin = origin;
+    }
+    // =============================================================
+    
+    // Setter(s) =======================
+    // ID
+    public void setID(int id) {
+        this.id = id;
+    }
+    
+    // Name
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    // Based On
+    public void setBasedOn(String basedOn) {
+        this.basedOn = basedOn;
+    }
+    
+    // Origin
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+    // =================================
+    
+    // Getter(s) ===============
+    // ID
+    public int getID() {
+        return id;
+    }
+    
+    // Name
+    public String getName() {
+        return name;
+    }
+    
+    // Based On
+    public String getBasedOn() {
+        return basedOn;
+    }
+    
+    // Origin
+    public String getOrigin() {
+        return origin;
+    }
+    // =========================
+    
+    // Method(s) ===========================================================================
+    // Linux Distro Information
+    public String getInformation() {
+        return this.name + " [Based On: " + this.basedOn + ", Origin: " + this.origin + "]";
+    }
+    // =====================================================================================
+}
